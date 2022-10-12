ba41dad9833b8d423cf3a09b1f86ec2eac478f97
Devin Spikowski <vegeta897@gmail.com>
2022-10-12 05:00:58 -0400

    🗃️ Add db wrapper for safer reads and writes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
 import { days, setCookie } from '$lib/server/cookies'
-import { db } from '$lib/server/db'
+import { getData, modifyData } from '$lib/server/db'
 import { connectBot } from '$lib/server/discord/bot'
 import type { Handle } from '@sveltejs/kit'
 
@@ -8,15 +8,13 @@ await connectBot()
 export const handle: Handle = async ({ event, resolve }) => {
 	console.log('begin handle', event.routeId)
 	let session = event.cookies.get('wec-session')
-	let storedSession =
-		session && db.data!.sessions.find((s) => s.sessionID === session)
+	const sessions = getData().sessions
+	let storedSession = session && sessions.find((s) => s.sessionID === session)
 	if (storedSession) console.log('found stored session')
 	const now = Date.now()
-	let dbModified = false
 	if (storedSession && storedSession.expires <= now) {
 		console.log('stored session is expired')
-		db.data!.sessions = db.data!.sessions.filter((s) => s.expires > now)
-		dbModified = true
+		modifyData({ sessions: sessions.filter((s) => s.expires > now) })
 		session = undefined
 		storedSession = undefined
 	}
@@ -26,11 +24,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 		setCookie(event.cookies, 'wec-session', session, {
 			expires: days(30),
 		})
-		db.data!.sessions.push({ sessionID: session, expires: days(30).getTime() })
-		dbModified = true
+		modifyData({
+			sessions: [
+				...sessions,
+				{ sessionID: session, expires: days(30).getTime() },
+			],
+		})
 	}
-	// TODO: Create wrapper to handle writes in a queue to avoid conflicts
-	if (dbModified) db.write()
 	event.locals.session = session
 	return await resolve(event)
 }
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,12 @@
 import { Low, JSONFile } from 'lowdb'
 
+// https://stackoverflow.com/a/59700012/2612679
+type DeepReadonly<T> = T extends Function
+	? T
+	: T extends object
+	? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+	: T
+
 type DBData = {
 	sessions: Session[]
 } & Record<string, any>
@@ -12,9 +19,30 @@ type Session = {
 
 // Use JSON file for storage
 const adapter = new JSONFile<DBData>('./db.json')
-
-export const db = new Low<DBData>(adapter)
-
+const db = new Low<DBData>(adapter)
 await db.read()
-
 db.data ||= { users: [], sessions: [] }
+
+export function getData(): DeepReadonly<DBData> {
+	return db.data!
+}
+
+export function modifyData(data: Partial<DBData>) {
+	db.data = <DBData>{ ...db.data, ...data }
+	queueWriteData()
+}
+
+let dirty = false
+function queueWriteData() {
+	dirty = true
+	setTimeout(writeData)
+}
+
+let writing = false
+async function writeData() {
+	if (!dirty || writing) return
+	writing = true
+	await db.write()
+	writing = false
+	dirty = false
+}
