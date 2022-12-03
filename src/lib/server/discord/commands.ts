cfae6d67ba1b1d1a4b6a7ab3e49e760cec8ee910
Devin Spikowski <vegeta897@gmail.com>
2022-12-02 21:21:30 -0500

    👔 Update /whenever command

--- src/lib/server/discord/commands.ts
+++ src/lib/server/discord/commands.ts
@@ -4,6 +4,7 @@ import {
 	DISCORD_CLIENT_ID,
 } from '$env/static/private'
 import { Client, REST, Routes, SlashCommandBuilder } from 'discord.js'
+import { DateTime } from 'luxon'
 import { getVotes } from '../db'
 
 const commands = [
@@ -28,28 +29,42 @@ export async function registerCommands() {
 function getCalendarStats() {
 	const votes = getVotes()
 	const days = [...new Set(votes.map((v) => v.YYYYMMDD))]
-	const mostVotedDay = days.reduce((most, current) => {
-		if (
-			votes.filter((v) => v.YYYYMMDD === current).length >
-			votes.filter((v) => v.YYYYMMDD === most).length
-		) {
-			return current
-		} else {
-			return most
-		}
-	}, days[0])
 	const users = [...new Set(votes.map((v) => v.userID))]
-	return `The calendar has **${votes.length}** votes on **${days.length}** days from **${users.length}** users
-${mostVotedDay} has the most votes`
+	let stats = `The calendar has **${votes.length}** votes on **${days.length}** days from **${users.length}** users`
+
+	let mostVotesOnAnyDay = 0
+	let mostVotedDays: string[] = []
+	for (const day of days) {
+		const voteCount = votes.filter((v) => v.YYYYMMDD === day).length
+		if (voteCount > mostVotesOnAnyDay) {
+			mostVotedDays.length = 0
+			mostVotesOnAnyDay = voteCount
+		}
+		if (voteCount === mostVotesOnAnyDay) {
+			mostVotedDays.push(day)
+		}
+	}
+	mostVotedDays = mostVotedDays.map((day) =>
+		DateTime.fromFormat(day, 'yyyy-LL-dd').toFormat('MMM d')
+	)
+	if (mostVotedDays.length === 1) {
+		stats += `\n**${mostVotedDays[0]}** has the most votes (${mostVotesOnAnyDay})`
+	} else if (mostVotedDays.length === 2) {
+		stats += `\n**${mostVotedDays[0]}** and **${mostVotedDays[1]}** have the most votes (${mostVotesOnAnyDay} each)`
+	} else if (mostVotedDays.length === 3) {
+		stats += `\n**${mostVotedDays[0]}**, **${mostVotedDays[1]}** and **${mostVotedDays[2]}** have the most votes (${mostVotesOnAnyDay} each)`
+	}
+	return stats
 }
 
-// setTimeout(() => console.log(getCalendarStats()), 5000)
+setTimeout(() => console.log(getCalendarStats()), 5000)
 
 export function handleCommands(bot: Client) {
 	bot.on('interactionCreate', async (interaction) => {
 		if (!interaction.isChatInputCommand()) return
 		if (interaction.commandName === 'whenever') {
-			const status = `**Whenever**
+			const status = `**Vote on the days you can play Sven!**
+
 ${getCalendarStats()}
 
 ${APP_URL}`
